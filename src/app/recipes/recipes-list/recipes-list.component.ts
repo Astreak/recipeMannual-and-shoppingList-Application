7264f9be61d968e76c9acd19fed925a8cb96b792
praj000 <parajbhattacharjee123@gmail.com>
2020-10-25 14:17:59 +0530

    Angular Application

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipesListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe("The first", "Ok this is the first entry","https://images-gmi-pmc.edge-generalmills.com/7eb09e32-fb06-4a34-97e6-def91b62164f.jpg")
+    new Recipe("The first", "Ok this is the first entry", "https://images-gmi-pmc.edge-generalmills.com/7eb09e32-fb06-4a34-97e6-def91b62164f.jpg"),
+    new Recipe("The hotdog", "Hello i love hot dogs","https://i.ndtvimg.com/i/2016-10/hot-dog-620_620x350_71476862261.jpg")
   ];
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient("Apple", 12),
+    new Ingredient("Pear", 2)
+  ];
   constructor() { }
 
   ngOnInit(): void {
